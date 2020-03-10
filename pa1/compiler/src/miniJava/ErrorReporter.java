e19162910cfc40433fce239e033c4f8db16ee553
Aidan Zimmermann <aidan.zimmermann@gmail.com>
2020-03-09 21:12:10 -0400

    PA3: added traveler interface

---
+++ pa1/compiler/src/miniJava/ErrorReporter.java
@@ -0,0 +1,21 @@
+package miniJava;
+
+import miniJava.SyntacticAnalyzer.SourcePosition;
+
+public class ErrorReporter {
+
+	int numErrors;
+
+	ErrorReporter() {
+		this.numErrors = 0;
+	}
+
+	public void reportError(String message, String tokenName, SourcePosition pos) {
+		//System.out.println("ERROR: " + message);
+		this.numErrors++;
+	}
+
+	public void reportRestriction(String message) {
+		System.out.println("RESTRICTION: " + message);
+	}
+}
